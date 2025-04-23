c9be88b0aba4e9abd1a8c09a2df6df4bacc7bc5e
sohanmanju <sohan@sohanmanju.com>
2025-04-23 23:57:20 +0530

    okay now ditched it

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -2,9 +2,8 @@
 
 import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
-import { cva, type VariantProps } from "class-variance-authority"
 
-import { cn } from "@/lib/utils.ts"
+import { cn } from "./utils.ts"
 
 function Label({
   className,
